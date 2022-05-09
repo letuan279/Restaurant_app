3eb5ba8c7f99cb6af3044f8f6438874332123e99
Le_Tuan <latuan388@gmail.com>
2022-05-09 17:55:35 +0700

    add: connected Mysql local and crud restaurant api

--- restaurant_server/app/Http/Controllers/API/RestaurantController.php
+++ restaurant_server/app/Http/Controllers/API/RestaurantController.php
@@ -8,6 +8,14 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
+    public function index(){
+        $restaurants = Restaurant::all();
+        return response()->json([
+            'status' => 200,
+            'restaurants' => $restaurants,
+        ]);
+    }
+
     public function store(Request $request)
     {
         $restaurant = new Restaurant;
@@ -22,4 +30,49 @@ class RestaurantController extends Controller
             'message' => 'Restaurant created Successfully',
         ]);
     }
+
+    public function update(Request $request, $id){
+        $restaurant = Restaurant::find($id);
+        if($restaurant){
+            $restaurant->name = $request->input('name');
+            $restaurant->description = $request->input('description');
+            $restaurant->address = $request->input('address');
+            $restaurant->image = $request->input('image');
+            $restaurant->update();
+
+            return response()->json([
+                'status'=>200,
+                'message'=>'Restaurant updated successfully',
+                'restaurant'=>$restaurant,
+            ]);
+        }
+        else {
+            return response()->json([
+                'status'=>404,
+                'message'=>'Restaurant not found',
+            ]);
+        }
+
+    }
+
+    public function destroy($id){
+        $restaurant = Restaurant::find($id);
+        if($restaurant){
+            $restaurant->delete();
+
+            return response()->json([
+                'status'=>200,
+                'message'=>'Restaurant deleted successfully',
+                'restaurant'=>$restaurant,
+            ]);
+        }
+        else {
+            return response()->json([
+                'status'=>404,
+                'message'=>'Restaurant not found',
+            ]);
+        }
+    }
+
+
 }
--- restaurant_server/routes/api.php
+++ restaurant_server/routes/api.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\API\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/get-restaurants', [RestaurantController::class, 'index']);
+Route::post('/add-restaurant', [RestaurantController::class, 'store']);
+Route::put('/update-restaurant/{id}', [RestaurantController::class, 'update']);
+Route::delete('/delete-restaurant/{id}', [RestaurantController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
